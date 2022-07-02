ed4809fa626d0ae868a0aa9ff8071e004f5b805a
irmansyaah <irmansyaah05@gmail.com>
2022-07-02 15:54:33 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style_beranda.css">
+  <title>Portofolio | Irmansyah</title>
+</head>
+
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero">
+            <h1><span>Portofolio</span><span></span></h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Service">AboutMe</a></li>
+            <!-- <li><a href="#projects" data-after="Projects">Projects</a></li>
+            <li><a href="#about" data-after="About">About</a></li> -->
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Hello, <span></span></h1>
+        <h1>My Name is <span></span></h1>
+        <h1>Irmansyah <span></span></h1>
+        <a href="#about" type="button" class="cta">Profil</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Service Section -->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">About<span>Me</span></h1>
+        <p>Perkenalkan nama saya Irmansyah, lulusan Universitas Putra Indonesia "YPTK" Padang jurusan Sistem Informasi. Saya memiliki kemauan belajar dalam bidang IT terkhusus tentang Web Programming, Front End Developer dan Data Engineer, sampai kapanpun akan terus belajar upgrade kemampuan dalam bidang tersebut.
+          Saya juga pernah mengikuti les private bersetifikat Web Programming dan pelatihan bersetifikat Data Engineer.
+        </p>
+      </div>
+      <!-- <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+      </div> -->
+    </div>
+  </section>
+  <!-- End Service Section -->
+
+  <!-- Projects Section -->
+  <!-- <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">Recent <span>Projects</span></h1>
+      </div>
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 1</h1>
+            <h2>Coding is Love</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 2</h1>
+            <h2>Coding is Love</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 3</h1>
+            <h2>Coding is Love</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 4</h1>
+            <h2>Coding is Love</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 5</h1>
+            <h2>Coding is Love</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> -->
+  <!-- End Projects Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="./img/bg.JPG" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">Profi<span>le</span></h1>
+        <h2>------------------------------------------------------------------------------</h2>
+        <h2>Nama : IRMANSYAH</h2>
+        <h2>Tempat/Tgl.Lahir : Padang/O5 Desember 1997</h2>
+        <h2>Alamat : <Address>Teluk Bayur, Padang Selatan, Kota Padang, Sumatera Barat.</Address></h2>
+        <h2>------------------------------------------------------------------------------</h2>
+        <h2>Pendidikan :</h2>
+        <h2>Strata 1, Sistem Informasi, Fakultas Ilmu Komputer, Universitas Putra Indonesia "YPTK" Padang.</h2>
+        <h2>------------------------------------------------------------------------------</h2>
+        <h2>Pengalaman Kerja :</h2>
+        <h2>Freelance Barista - Bengras Kopi Padang / 2021-2022</h2>
+        <h2>Staff Gudang - CV Sinar Damai Padang / 2016-2017</h2>
+        <h2>------------------------------------------------------------------------------</h2>
+        <h2>Private / Pelatian bersetifikat :</h2>
+        <h2>Web Programming - CV. Mediatama Web / 2021-2022</h2>
+        <h2>Data Engineer - Digitalent Kominfo (Mitra DQLab) / 2022</h2>
+        <h2>------------------------------------------------------------------------------</h2>
+        <!-- <a href="#" class="cta">Download Resume</a> -->
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/ios/100/000000/apple-phone.png" /></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>+62 813-7464-8472</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-email-cyber-security-kiranshastry-lineal-kiranshastry-3.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>irmansyaah05@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/ios/50/000000/linkedin.png" /></div>
+          <div class="contact-info">
+            <h1>LinkedIn</h1>
+            <h2>Irmansyah.</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+        <div class="footer container">
+            <div class="brand">
+                <!-- <h1><span></span>Irmansyah<span></span></h1> -->
+            </div>
+            <h2>Hubungi Media Sosial</h2>
+            <div class="social-icon">
+                <div class="social-item">
+                    <a href="#"><img src="https://img.icons8.com/ios/50/000000/facebook--v2.png" /></a>
+                    <p>Irmansyah12</p>
+                </div>
+                <div class="social-item">
+                    <a href="#"><img
+                            src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/64/000000/external-instagram-web-and-social-media-flatart-icons-outline-flatarticons.png" /></a>
+                    <p>Irmansyah126</p>
+                </div>
+                <div class="social-item">
+                    <a href="#"><img src="https://img.icons8.com/dotty/80/000000/telegram-app.png" /></a>
+                    <p>Irmansyah</p>
+                </div>
+                <div class="social-item">
+                    <a href="#"><img src="https://img.icons8.com/ios/50/000000/twitter-squared.png" /></a>
+                    <p>Mansyah</p>
+                </div>
+                <p>Terima Kasih Telah Berkunjung, Copyright &copy;2022, Irmansyah.</p>
+            </div>
+        <div>
+  </section>
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+</body>
+
+</html>
